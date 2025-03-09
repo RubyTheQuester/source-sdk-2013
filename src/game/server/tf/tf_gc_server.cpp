a0b5ef3572938e4cf83dff12995c08aea7d26d93
RubyTheQuester <jammylammer@gmail.com>
2025-03-09 01:50:37 -0500

    plus steve who got lost

--- src/game/server/tf/tf_gc_server.cpp
+++ src/game/server/tf/tf_gc_server.cpp
@@ -4388,9 +4388,6 @@ void CTFGCServerSystem::SDK_ApplyLocalLoadout(CGCClientSharedObjectCache* pCache
 			CEconItem soIndex;
 			soIndex.SetItemID(uItemId);
 
-			pTFInventory->EquipLocal(uItemId, iClass, iSlot);
-
-			/*
 			CEconItem* pItem = (CEconItem*) pItemCache->FindSharedObject(soIndex);
 			if (pItem) {
 				pTFInventory->EquipLocal(uItemId, iClass, iSlot);
@@ -4398,7 +4395,6 @@ void CTFGCServerSystem::SDK_ApplyLocalLoadout(CGCClientSharedObjectCache* pCache
 			else {
 				Warning("Failed to find item %llu in shared object, but client says it should be equipped by [%i] in slot [%i].\n", uItemId, iClass, iSlot);
 			}
-			*/
 		}
 	}
 }
--- src/game/shared/econ/econ_item_schema.cpp
+++ src/game/shared/econ/econ_item_schema.cpp
@@ -2306,7 +2306,6 @@ m_bIsPackBundle( false ),
 m_pOwningPackBundle( NULL ),
 m_bIsPackItem( false ),
 m_bBaseItem( false ),
-m_bSoloItem(false),
 m_pszItemLogClassname( NULL ),
 m_pszItemIconClassname( NULL ),
 m_pszDatabaseAuditTable( NULL ),
@@ -3178,7 +3177,6 @@ bool CEconItemDefinition::BInitFromKV( KeyValues *pKVItem, CUtlVector<CUtlString
 	m_bHidden = m_pKVItem->GetInt( "hidden", 0 ) != 0;
 	m_bShouldShowInArmory = m_pKVItem->GetInt( "show_in_armory", 0 ) != 0;
 	m_bBaseItem = m_pKVItem->GetInt( "baseitem", 0 ) != 0;
-	m_bSoloItem = m_pKVItem->GetInt("soloitem", 0) != 0;
 	m_pszItemLogClassname = m_pKVItem->GetString( "item_logname", NULL );
 	m_pszItemIconClassname = m_pKVItem->GetString( "item_iconname", NULL );
 	m_pszDatabaseAuditTable = m_pKVItem->GetString( "database_audit_table", NULL );
@@ -3807,7 +3805,6 @@ CEconItemSchema::CEconItemSchema( )
 ,	m_mapToolsItems( DefLessFunc(int) )
 ,	m_mapPaintKitTools( DefLessFunc(uint32) )
 ,	m_mapBaseItems( DefLessFunc(int) )
-,	m_mapSoloItems( DefLessFunc(int) )
 ,	m_unVersion( 0 )
 #if defined(CLIENT_DLL) || defined(GAME_DLL)
 ,	m_pDefaultItemDefinition( NULL )
@@ -4302,7 +4299,6 @@ void CEconItemSchema::Reset( void )
 	m_mapToolsItems.Purge();
 	m_mapPaintKitTools.Purge();
 	m_mapBaseItems.Purge();
-	m_mapSoloItems.Purge();
 	m_mapRecipes.PurgeAndDeleteElements();
 	m_vecTimedRewards.Purge();
 	m_dictItemSets.PurgeAndDeleteElements();
@@ -4422,7 +4418,7 @@ bool CEconItemSchema::BInitTextBuffer( CUtlBuffer &buffer, CUtlVector<CUtlString
 
 	Reset();
 	m_pKVRawDefinition = new KeyValues( "CEconItemSchema" );
-	if (m_pKVRawDefinition->LoadFromFile(g_pFullFileSystem, "scripts/items/items_custom.txt", "GAME"))
+	if ( m_pKVRawDefinition->LoadFromBuffer( NULL, buffer ) )
 	{
 		return BInitSchema( m_pKVRawDefinition, pVecErrors )
 			&& BPostSchemaInit( pVecErrors );
@@ -5277,7 +5273,6 @@ bool CEconItemSchema::BInitItems( KeyValues *pKVItems, CUtlVector<CUtlString> *p
 	m_mapToolsItems.Purge();
 	m_mapPaintKitTools.Purge();
 	m_mapBaseItems.Purge();
-	m_mapSoloItems.Purge();
 	m_vecBundles.Purge();
 	m_mapQuestObjectives.PurgeAndDeleteElements();
 
@@ -5348,10 +5343,6 @@ bool CEconItemSchema::BInitItems( KeyValues *pKVItems, CUtlVector<CUtlString> *p
 					m_mapBaseItems.Insert( nItemIndex, pItemDef );
 				}
 
-				if (pItemDef->IsSoloItem())
-				{
-					m_mapSoloItems.Insert(nItemIndex, pItemDef);
-				}
 				// Cache off bundles for the link phase below.
 				if ( pItemDef->IsBundle() )
 				{
