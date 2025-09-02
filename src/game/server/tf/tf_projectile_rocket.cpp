f648b29b504cfd841e16b2ed7507fd4a990ff939
RubyTheQuester <jammylammer@gmail.com>
2025-09-02 05:05:20 -0400

    fixed some things + set up current rocket lanuchers to use new models

--- src/game/server/tf/tf_projectile_rocket.cpp
+++ src/game/server/tf/tf_projectile_rocket.cpp
@@ -73,7 +73,7 @@ void CTFProjectile_Rocket::Precache()
 	iModel = PrecacheModel("models/enhancedrockets2/w_rocket_original.mdl");
 	iModel = PrecacheModel("models/enhancedrockets2/w_rocket_libertylauncher.mdl");
 	iModel = PrecacheModel("models/enhancedrockets2/w_rocket_rocketjumper.mdl");
-	iModel = PrecacheModel("models/enhancedrockets2/w_rocket_beggarsbazookab.mdl");
+	iModel = PrecacheModel("models/enhancedrockets2/w_rocket_beggarsbazooka.mdl");
 
 	BaseClass::Precache();
 }
